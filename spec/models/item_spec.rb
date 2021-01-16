cdc0a9be4b8c2d36ec25677d5764c5af6819de78
kotaro1020 <tmcz4la8t@gmail.com>
2021-01-16 21:25:19 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -60,12 +60,12 @@ RSpec.describe Item, type: :model do
       it 'priceが300円以下では登録できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than 300")
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it 'priceが10000000以上では登録できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than 9999999")
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it 'priceが全角数字では登録できない' do
         @item.price = '５００'
