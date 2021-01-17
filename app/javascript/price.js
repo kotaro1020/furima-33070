9cfc23b917e9b02fbfaf9278e2815768a55b1390
kotaro1020 <tmcz4la8t@gmail.com>
2021-01-17 12:21:41 +0900

    console.log削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,7 +3,6 @@ window.addEventListener('load', () => {
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  // console.log(inputValue);
 
 
     const addTaxDom = document.getElementById("add-tax-price");
