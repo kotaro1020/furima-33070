9ab026f1b1a439dae258a2a3b87cdafef9e204b5
kotaro1020 <tmcz4la8t@gmail.com>
2021-01-17 20:32:51 +0900

    all削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all.order(created_at: :DESC)
+    @items = Item.order(created_at: :DESC)
   end
   def new
     @item = Item.new
