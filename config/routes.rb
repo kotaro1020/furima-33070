c1c9e8c6901ba2c0bb80458fbaf2ea4dfe245241
kotaro1020 <tmcz4la8t@gmail.com>
2021-01-19 21:14:33 +0900

    商品削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:index ,:new, :create, :show, :edit, :update] do
+  resources :items, only: [:index ,:new, :create, :show, :edit, :update, :destroy] do
   end
 end
