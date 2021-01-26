8c00f1d274b579d9614ecc4b2561aae0c4dfb8b6
kotaro1020 <tmcz4la8t@gmail.com>
2021-01-26 17:59:02 +0900

    商品購入テストコード

--- spec/models/pay_form_spec.rb
+++ spec/models/pay_form_spec.rb
@@ -20,62 +20,60 @@ RSpec.describe PayForm, type: :model do
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Municipality can't be blank")
       end
-      it 'addressが空では登録できない' do
+      it 'addressが空では購入できない' do
         @pay_form.address = ''
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Address can't be blank")
       end
-      it 'tokenが空では登録できない' do
+      it 'tokenが空では購入できない' do
         @pay_form.token = nil
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Token can't be blank")
       end
-      it 'postalcodeが空では登録できない' do
+      it 'postalcodeが空では購入できない' do
         @pay_form.postalcode = ''
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Postalcode can't be blank")
       end
-      it 'postalcodeはhyphen(-)がないと登録できない' do
+      it 'postalcodeはhyphen(-)がないと購入できない' do
         @pay_form.postalcode = 1111111
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Postalcode is invalid. Include hyphen(-)")
       end
-      it 'region_idが「---」では登録できない' do
+      it 'region_idが「---」では購入できない' do
         @pay_form.region_id = 1
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Region must be other than 1")
       end
-      it 'phonenumberが空では登録できない' do
+      it 'phonenumberが空では購入できない' do
         @pay_form.phonenumber = ''
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Phonenumber can't be blank")
       end
-      it 'phonenumberが全角では登録できない' do
+      it 'phonenumberが全角では購入できない' do
         @pay_form.phonenumber = '０９０１１１１１１１１'
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Phonenumber is invalid")
       end
-      it 'phonenumberが10文字では登録できない' do
-        @pay_form.phonenumber = '0901111111'
-        @pay_form.valid?
-        expect(@pay_form.errors.full_messages).to include("Phonenumber is invalid")
-      end
-      it 'phonenumberが12文字では登録できない' do
+      it 'phonenumberが12文字では購入できない' do
         @pay_form.phonenumber = '090111111122'
         @pay_form.valid?
         expect(@pay_form.errors.full_messages).to include("Phonenumber is invalid")
       end
-      it 'phonenumberが英語では登録できない' do
+      it 'phonenumberが英語では購入できない' do
         @pay_form.phonenumber = 'aaaaaaaaaaa'
         @pay_form.valid?
-        binding.pry
         expect(@pay_form.errors.full_messages).to include("Phonenumber is invalid")
       end
-      it "ユーザーが紐付いていないとツイートは保存できない" do
-        @pay_form.@user.id = nil
+      it "ユーザーが紐付いていないと購入できない" do
+        @pay_form.user_id = nil
         @pay_form.valid?
-        binding.pry
-        expect(@pay_form.errors.full_messages).to include("User must exist")
+        expect(@pay_form.errors.full_messages).to include("User can't be blank")
+      end
+      it "商品が紐付いていないと購入できない" do
+        @pay_form.item_id = nil
+        @pay_form.valid?
+        expect(@pay_form.errors.full_messages).to include("Item can't be blank")
       end
     end
   end
